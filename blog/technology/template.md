378bb1a803dac60e270f284016ea1b4b8a9aa5be
fwh-mac-mini <fuwenhao594@163.com>
2024-03-27 15:02:02 +0800

    fix: 文章展示

---
+++ blog/technology/template.md
@@ -0,0 +1,12 @@
+---
+slug: ai-template
+title: AI检索引擎
+date: 2008-03-27
+authors: wenhao
+tags: [develop, cloud]
+keywords: [develop, cloud]
+# image: /img/blog/vue-chrome-extension.png
+# sticky: 6
+---
+![image-20210905085929546](/img/blog/vue-chrome-extension.png)
+<!-- truncate -->
--- data/friend.ts
+++ data/friend.ts
@@ -1,10 +1,10 @@
 export const Friends: Friend[] = [
-  {
-    title: '峰华前端工程师',
-    description: '致力于帮助你以最直观、最快速的方式学会前端开发',
-    website: 'https://zxuqian.cn',
-    avatar: '/img/friend/zxuqian.png',
-  },
+  // {
+  //   title: '峰华前端工程师',
+  //   description: '致力于帮助你以最直观、最快速的方式学会前端开发',
+  //   website: 'https://zxuqian.cn',
+  //   avatar: '/img/friend/zxuqian.png',
+  // },
   {
     title: 'Y2bmp3',
     description: 'YouTube to MP3',
